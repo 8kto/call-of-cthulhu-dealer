3efe228550381f62fd8984c7f89923e8c9c773ec
Okto <web@axisful.info>
2023-02-20 23:56:18 +0100

    Formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
 
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
+}
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-};
+}
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
@@ -18,9 +18,9 @@ const IndexPage: React.FC<PageProps> = () => {
       <h1 style={headingStyles}>TODO</h1>
       <p>TODO</p>
     </main>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Home Page</title>
--- src/pages/showcase.tsx
+++ src/pages/showcase.tsx
@@ -1,16 +1,16 @@
-import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
 
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
+}
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-};
+}
 
 const ShowcasePage: React.FC<PageProps> = () => {
   return (
@@ -18,9 +18,9 @@ const ShowcasePage: React.FC<PageProps> = () => {
       <h1 style={headingStyles}>Showcase</h1>
       <p>TODO</p>
     </main>
-  );
-};
+  )
+}
 
-export default ShowcasePage;
+export default ShowcasePage
 
-export const Head: HeadFC = () => <title>Showcase</title>;
+export const Head: HeadFC = () => <title>Showcase</title>
