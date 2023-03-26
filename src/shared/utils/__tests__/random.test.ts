f1ed3570d779ae68dc48a38fe09931846523857b
Okto <web@axisful.info>
2023-03-26 20:04:13 +0200

    Test lib call

--- src/shared/utils/__tests__/random.test.ts
+++ src/shared/utils/__tests__/random.test.ts
@@ -1,9 +1,29 @@
+import { Random } from 'random-js'
 import { roll } from 'shared/utils/random'
+import { Dice } from 'types'
+
+jest.mock('random-js', () => ({
+  Random: class {
+    integer() {
+      return 1
+    }
+  },
+}))
 
 describe('random helpers', () => {
   describe('roll', () => {
-    it('should return number', () => {
-      expect(typeof roll()).toEqual('number')
+    it('should call library', () => {
+      const integerSpy = jest.spyOn(Random.prototype, 'integer')
+      roll()
+
+      expect(integerSpy).toHaveBeenCalledWith(1, 100)
+    })
+
+    it('should call library for the passed diapason', () => {
+      const integerSpy = jest.spyOn(Random.prototype, 'integer')
+      roll(Dice.d6)
+
+      expect(integerSpy).toHaveBeenCalledWith(1, Dice.d6)
     })
   })
 })
