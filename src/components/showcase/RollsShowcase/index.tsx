c88107e5dd6b5ff5ffcc89f466bce973a5ce2c78
Okto <web@axisful.info>
2023-04-04 00:42:56 +0200

    Drop local state use global

--- src/components/showcase/RollsShowcase/index.tsx
+++ src/components/showcase/RollsShowcase/index.tsx
@@ -10,23 +10,28 @@ import { useAppDispatch, useAppSelector } from 'shared/hooks'
 import './styles.scss'
 
 const RollsShowcase = () => {
-  const [resultD100, setResultD100] = useState<number>()
-  const [result, setResult] = useState<number>()
   const [difficulty, setDifficulty] = useState<number>(50)
   const [log, setLog] = useState<Array<string>>([])
 
-  const count = useAppSelector(selectDiceTrayValue)
+  const result = useAppSelector(selectDiceTrayValue)
   const dispatch = useAppDispatch()
 
   const addLog = (msg: string) => {
     setLog(prevState => [...prevState, msg])
   }
 
+  const dispatchResult = (dice: Dice) => {
+    dispatch(setTrayValue(roll(dice)))
+  }
+
   return (
     <div className="column">
       <section className="box mb-4">
+        <h2>Result: {result || ''}</h2>
+        <h3 className="subtitle">{result && getThrowResultAsString(result, difficulty)}</h3>
+      </section>
+      <section className="box mb-4">
         <h2>Roll d100</h2>
-        Count: <span>{count}</span>
         <div className="field">
           <label className="label">Enter the difficulty</label>
           <div className="control">
@@ -48,8 +53,7 @@ const RollsShowcase = () => {
           className="button is-primary"
           onClick={() => {
             const res = roll()
-            dispatch(setTrayValue(res))
-            setResultD100(res)
+            dispatchResult(Dice.d100)
             addLog(
               `Roll ${res} against DC ${difficulty}; result is ${getThrowResultAsString(
                 res,
@@ -60,30 +64,23 @@ const RollsShowcase = () => {
         >
           Roll d100
         </button>
-        <p className="mt-2">
-          Result: <strong>{resultD100 || ''}</strong>(
-          {resultD100 && getThrowResultAsString(resultD100, difficulty)})
-        </p>
       </section>
-
       <section className="box mb-4">
         <h2 className="title">Roll other dices</h2>
-        <h3 className="subtitle">Result: {result || ''}</h3>
-
         <div className="buttons">
-          <button type="button" className="button" onClick={() => setResult(roll(Dice.d20))}>
+          <button type="button" className="button" onClick={() => dispatchResult(Dice.d20)}>
             Roll d20
           </button>
-          <button type="button" className="button" onClick={() => setResult(roll(Dice.d10))}>
+          <button type="button" className="button" onClick={() => dispatchResult(Dice.d10)}>
             Roll d10
           </button>
-          <button type="button" className="button" onClick={() => setResult(roll(Dice.d6))}>
+          <button type="button" className="button" onClick={() => dispatchResult(Dice.d6)}>
             Roll d6
           </button>
-          <button type="button" className="button" onClick={() => setResult(roll(Dice.d4))}>
+          <button type="button" className="button" onClick={() => dispatchResult(Dice.d4)}>
             Roll d4
           </button>
-          <button type="button" className="button" onClick={() => setResult(roll(Dice.d2))}>
+          <button type="button" className="button" onClick={() => dispatchResult(Dice.d2)}>
             Roll d2
           </button>
         </div>
